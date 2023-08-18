9ec17219cb44abf740288ea351d0e7e7154ad98d
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-18 17:11:24 +0300

    Addition of user.rb validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   def new_posts
     posts.order(created_at: :asc).limit(3)
   end
+
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true },
+                            comparison: { greater_than_or_equal_to: 0 }
 end
