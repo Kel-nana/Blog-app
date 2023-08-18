541bfcfdcc787e4d35f80aa1a97a4cd0b64713cb
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-18 17:10:37 +0300

    Addition of post.rb validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,11 @@ class Post < ApplicationRecord
   end
 
   def new_comments
-    comments.order(created_at: :asc).limit(5)
+    comments.order(created_at: :desc).limit(5)
   end
+
+  # validates :title, length: { maximum: 250 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
