f0d2f876ad1caf6201a0bf7b81da49d75af4f1f7
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-28 13:34:25 +0300

    Moving validations to run before model methods and fixnig linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
 
-  def new_posts
-    posts.order(created_at: :asc).limit(3)
-  end
-
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true },
                             comparison: { greater_than_or_equal_to: 0 }
+
+  def new_posts
+    posts.order(created_at: :asc).limit(3)
+  end
 end
