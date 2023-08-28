ce76748deee7e8e3013ec060fb96d935d0c3f5c7
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-28 12:50:40 +0300

    Addition of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,85 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root 'users#index'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index ,:show]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# root 'users#index'
+  # get "users", to: "users#index"
+  # get "users/:id", to: "users#show"
+  # get "users/:user_id/posts", to: "posts#index"
+  # get "users/:user_id/posts/:id", to: "posts#show"
