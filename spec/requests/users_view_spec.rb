168db33c3240dd379c4fdfe21b272e4d7a3704e3
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-28 18:37:04 +0300

    Fixing linter errors

--- spec/requests/users_view_spec.rb
+++ spec/requests/users_view_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "users/index", type: :view do
-  it "displays 'Users list' heading" do
-    assign(:users, [User.new(name: 'user1'), User.new(name: 'user2')]) 
+RSpec.describe 'users/index', type: :view do
+  it 'displays "Users list" heading' do
+    assign(:users, [User.new(name: 'user1'), User.new(name: 'user2')]) # Set up dummy users data
     render
     expect(rendered).to have_selector('h1', text: 'Users list')
   end
