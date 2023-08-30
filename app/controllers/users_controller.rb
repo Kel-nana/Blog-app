9ac587e125f275c500a1aef481e38870e82096c3
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-30 15:06:36 +0300

    Setting up controllers for the view and posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+    #  @users.each do |user|
+    #   puts "User ID: #{user.id}, Name: #{user.name}, Email: #{user.email}"
+    # end
+  end
 
-  def show; end
+  def show
+    @user = User.find_by_id(params[:id])
+    @posts = @user.new_posts
+  end
 end
