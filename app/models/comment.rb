0377f6dc73e2f34339ecbcc91825a9b5818f6193
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-30 15:03:09 +0300

    Edit of After_save to after_create to stop the callack on save

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  after_save :update_post_comments_counter
+
+  after_create :update_post_comments_counter
   after_destroy :update_post_comments_counter
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :posts_counter
 
-  after_save :update_user_posts_counter
+  after_create :update_user_posts_counter
 
   # validates :title, length: { maximum: 250 }
   validates :title, presence: true, length: { maximum: 250 }
@@ -11,7 +11,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_user_posts_counter
-    author.increment!(:posts_counter)
+    author.increment!(:posts_counter, 1)
   end
 
   def new_comments
