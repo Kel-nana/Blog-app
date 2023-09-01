98bba5ee7858da25550836471a21cc2dddbd9325
Kelvin Irungu <112465233+Kel-nana@users.noreply.github.com>
2023-09-01 19:04:30 +0300

    Fixing linter errors post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-
   after_commit :update_user_posts_counter, on: %i[create destroy]
 
   validates :title, presence: true, length: { maximum: 250 }
