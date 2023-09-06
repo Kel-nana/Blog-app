47e7bb9bde722f957f4a1639b99623f07732d310
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-09-06 22:17:03 +0300

    Addition of devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Method to retrieve the currently logged-in user
-  def current_user
-    @current_user = User.first
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :current_user
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :bio, :email, :password)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    end
   end
 
-  # Make the current_user method available to views
-  helper_method :current_user
+  # def current_user
+  #   @current_user ||= User.find(params[:user_id])
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
 
+  before_create :initialize_posts_counter
+
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true },
-                            comparison: { greater_than_or_equal_to: 0 }
 
   def new_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def initialize_posts_counter
+    self.posts_counter ||= 0
   end
 end
