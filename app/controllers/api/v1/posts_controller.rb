184848e84cfd6b207c61b1168456392aa5acf95d
Enoisong <enobongitiaba988@gmail.com>
2023-09-08 06:04:06 -0700

    add posts controller file

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::PostsController < ApplicationController
+    def index
+      user = User.find(params[:user_id])
+      posts = user.posts
+  
+      render json: posts, status: :ok
+    end
+  end
+  posts_controller.rb
